70a00ff94601fca967e2d390d807879ab482632c
Alexey <overlord1337zz@gmail.com>
2021-02-09 20:28:34 +0600

    Update CreateSnippet view's

--- src/sharing_code/views.py
+++ src/sharing_code/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from .forms import SnippetForm
 from .models import Snippet
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, FormView
 
-
-def createSnippet(request):
+"""def createSnippet(request):
     if request.method == 'POST':
         form = SnippetForm(request.POST)
         if form.is_valid():
@@ -15,6 +14,19 @@ def createSnippet(request):
         form = SnippetForm()
     context = {'form': form}
     return render(request, 'createSnippet.html', context)
+"""
+
+
+class CreateSnippet(FormView):
+    form_class = SnippetForm
+    template_name = 'createSnippet.html'
+
+    def form_valid(self, form):
+        if self.request.recaptcha_is_valid:
+            snippet = form.save(commit=False)
+            snippet.save()
+            return HttpResponseRedirect(reverse('snippet:detail', kwargs={'pk': snippet.id}), self.get_context_data())
+        return render(self.request, 'createSnippet.html', self.get_context_data())
 
 
 class ViewSnippet(DetailView):
