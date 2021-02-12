f93b8bcaa7ca8c68ddb5c0b6c46851b09595653a
Alexey <overlord1337zz@gmail.com>
2021-02-12 11:27:15 +0600

    update Admin panel

--- src/sharing_code/admin.py
+++ src/sharing_code/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Snippet
 
-admin.site.register(Snippet)
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'lang', 'date')
