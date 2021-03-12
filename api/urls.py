c06288176d37bd6c4cf0bc19148de218fb59b8ca
Alexey <overlord1337zz@gmail.com>
2021-03-12 16:34:31 +0600

    API 1.0.0

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.SnippetAPIView.as_view()),
-    path('snippet/<uuid:pk>', views.SnippetAPIView.as_view())
+    path('/<uuid:pk>', views.SnippetAPIView.as_view())
 ]
